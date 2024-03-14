493df7654be05dffeb3d12a409e8ada802107108
Michael Netzer <netzermi@gmail.com>
2024-03-14 13:40:46 +0100

    1st commit for gh actions

---
+++ src/main/java/at/htl/control/RestConfig.java
@@ -0,0 +1,9 @@
+package at.htl.control;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+    @ApplicationPath("api")
+    public class RestConfig extends Application {
+    }
+
